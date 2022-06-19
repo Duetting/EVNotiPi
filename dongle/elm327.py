092c1d365f52f0f8419d35ee5286756a67178a6a
Duetting <Duetting@users.noreply.github.com>
2022-06-19 11:37:43 +0200

    self bei init_dongle fehlte

--- dongle/elm327.py
+++ dongle/elm327.py
@@ -31,7 +31,7 @@ class Elm327(AtBase):
                 for cmd, exp in cmds:
                     self.send_at_cmd(cmd, exp)
 
-                AtBase.init_dongle()
+                AtBase.init_dongle(self)
 
                 if self.current_protocol:
                     self.setProtocol(self.current_protocol)
